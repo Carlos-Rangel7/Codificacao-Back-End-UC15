c54e617eb0ebc6bb35a3290c750352ab26cb27f6
Carlos Rangel <croi7726@gmail.com>
2022-11-14 14:26:07 -0300

    ER02 UC15

---
+++ Interfaces/IPessoaFisica.cs
@@ -0,0 +1,7 @@
+namespace Back_End_ER02.Interfaces
+{
+    public interface IPessoaFisica
+    {
+       bool ValidarDataNascimento (DateTime dataNasc);
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Back_End_ER02.Classes;
+
+PessoaFisica novaPF = new PessoaFisica();
+
+novaPF.nome = "Carlos Rangel";
+novaPF.cpf = "52029988851";
+
+Console.WriteLine(novaPF.nome);
+Console.WriteLine(novaPF.cpf);
+
+
+
+
+
+
+
+
