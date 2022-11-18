85bf8af1d1fc65c153eb908a81234ea7bcf78d07
Carlos Rangel <croi7726@gmail.com>
2022-11-17 21:05:31 -0300

    ER03 UC15

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@ PessoaFisica novaPF = new PessoaFisica();
 
 novaPF.nome = "Carlos Rangel";
 novaPF.cpf = "52029988851";
+novaPF.rendimento = 6600.5f;
 
-Console.WriteLine(novaPF.nome);
-Console.WriteLine(novaPF.cpf);
+// Console.WriteLine(novaPF.nome);
+// Console.WriteLine(novaPF.cpf);
+// Console.WriteLine(novaPF.rendimento);
+// Console.WriteLine(novaPF.CalcularImposto (1200));
+// Concatenação usa + e interpolação usa $
+
+//Pessoa Fisica ======================================================
+float ImpostoApagar = novaPF.CalcularImposto (novaPF.rendimento);
+Console.WriteLine(ImpostoApagar);
+Console.WriteLine(ImpostoApagar.ToString("C2"));
 
 
+// Pessoa Juridica ===================================================================
+PessoaJuridica novaPj = new PessoaJuridica ();
+Console.WriteLine(novaPj.CalcularImposto(6600.5f));
 
 
 
