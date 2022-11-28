cca65313731bd2ea78eed7c1b33de3169d254a63
Carlos Rangel <croi7726@gmail.com>
2022-11-27 22:23:25 -0300

    ERO5 UC15

--- Classes/PessoaJuridica.cs
+++ Classes/PessoaJuridica.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Back_End_ER02.Interfaces;
 
 namespace Back_End_ER02.Classes
@@ -24,9 +25,31 @@ namespace Back_End_ER02.Classes
             }
         }
 
-        public bool ValidarCnpj(string cnpj)
+        internal void ValidarCnpj()
         {
             throw new NotImplementedException();
+        }
+        
+        public bool ValidarCnpj(string cnpj)
+        {// \ digito e / caractere
+            bool retornoCnpjValido = Regex.IsMatch(cnpj, @"^(\d{14}) | (\d{2}. \d{3}. \d{3}/ \d{4}- \d{2}) $");
+
+            if (retornoCnpjValido)
+            {   string subStringCnpj14 = cnpj.Substring(8, 4);
+                
+                if ( subStringCnpj14 == "0001")
+               {
+                    return true;
+               }
+            }
+
+              string subStringCnpj18 = cnpj.Substring (11, 4);
+
+              if (subStringCnpj18 == "0001")
+              {
+                return true;
+              }
+                return false;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,53 +1,66 @@
 ﻿using Back_End_ER02.Classes;
 using Back_End_ER04.Classes;
 
-PessoaFisica novaPF = new PessoaFisica();
-Endereco novoEndPF = new Endereco();
 
-novaPF.nome = "Carlos Rangel";
-novaPF.cpf = "52029988851";
-novaPF.rendimento = 6600.5f;
-novaPF.dataNasc = new DateTime(2002, 12, 20);
+    // PessoaFisica novaPF = new PessoaFisica();
+    // Endereco novoEndPF = new Endereco();
 
-novoEndPF.Logradouro = "Av. Maria cursi";
-novoEndPF.Numero = 1131;
-novoEndPF.Complemento = "Casa";
-novoEndPF.EndComercial = false;
+    // novaPF.nome = "Carlos Rangel";
+    // novaPF.cpf = "52029988851";
+    // novaPF.rendimento = 6600.5f;
+    // novaPF.dataNasc = new DateTime(2002, 12, 20);
 
-novaPF.endereco = novoEndPF;
+    // novoEndPF.Logradouro = "Av. Maria cursi";
+    // novoEndPF.Numero = 1131;
+    // novoEndPF.Complemento = "Casa";
+    // novoEndPF.EndComercial = false;
 
-Console.WriteLine(@$"Nome:{novaPF.nome}
-Endereço: {novaPF.endereco.Logradouro}
-Numero: {novaPF.endereco.Numero}
-{novaPF.endereco.Complemento}
-{novaPF.endereco.EndComercial}");
+    // novaPF.endereco = novoEndPF;
 
+    // Console.WriteLine(@$"Nome:{novaPF.nome}
+    // Endereço: {novaPF.endereco.Logradouro}
+    // Numero: {novaPF.endereco.Numero}
+    // {novaPF.endereco.Complemento}
+    // {novaPF.endereco.EndComercial}");
 
+    // Console.WriteLine(novaPF.nome);
+    // Console.WriteLine(novaPF.cpf);
+    // Console.WriteLine(novaPF.rendimento);
+    // Console.WriteLine(novaPF.CalcularImposto (1200));
+    // Concatenação usa + e interpolação usa $
 
-
-
-// Console.WriteLine(novaPF.nome);
-// Console.WriteLine(novaPF.cpf);
-// Console.WriteLine(novaPF.rendimento);
-// Console.WriteLine(novaPF.CalcularImposto (1200));
-// Concatenação usa + e interpolação usa $
-
-//Pessoa Fisica ======================================================
-// float ImpostoApagar = novaPF.CalcularImposto (novaPF.rendimento);
-// Console.WriteLine(ImpostoApagar);
-// Console.WriteLine(ImpostoApagar.ToString("C2"));
+    //Pessoa Fisica ======================================================
+    // float ImpostoApagar = novaPF.CalcularImposto (novaPF.rendimento);
+    // Console.WriteLine(ImpostoApagar);
+    // Console.WriteLine(ImpostoApagar.ToString("C2"));
 
 
-// // Pessoa Juridica ===================================================================
-// PessoaJuridica novaPj = new PessoaJuridica ();
-// Console.WriteLine(novaPj.CalcularImposto(6600.5f));
+    // Pessoa Juridica ===================================================================
+    // PessoaJuridica novaPj = new PessoaJuridica ();
+    // Console.WriteLine(novaPj.CalcularImposto(6600.5f));
 
-// DateTime temp = new DateTime (2005, 01, 01);
-// Console.WriteLine(novaPF.ValidarDataNasc("01/01/2000"));
+    // DateTime temp = new DateTime (2005, 01, 01);
+    // Console.WriteLine(novaPF.ValidarDataNasc("01/01/2000"));
 
+    PessoaJuridica novaPj = new PessoaJuridica();
+    Endereco novoEndPj = new Endereco();
 
+    novaPj.nome = "Nome Pj ";
+    novaPj.razaoSocial = "Razão social PJ";
+    novaPj.cnpj = "00.476.645/0001-03";
 
+    novoEndPj.Logradouro = "Maria Cursi";
+    novoEndPj.Numero = 1131;
+    // Console.WriteLine(novaPj.ValidarCnpj("00.476.645/0001-03"));
 
+    novaPj.endereco = novoEndPj;
 
-
+    Console.WriteLine(@$"
+    Nome: {novaPj.nome}
+    Razão Social: {novaPj.razaoSocial}
+    CNPJ: {novaPj.cnpj} - Valido: {novaPj.ValidarCnpj(novaPj.cnpj)}
+    
+    ");
+    
+        
 
